e0d74bb8a4097780944ecaa24e21059ca9166aca
Matheus M. Reis <mmr94@live.com>
2021-06-11 21:19:40 -0300

    adicionando update do cliente

--- Loja/src/main/java/org/serratec/resource/ClienteResource.java
+++ Loja/src/main/java/org/serratec/resource/ClienteResource.java
@@ -2,6 +2,8 @@ package org.serratec.resource;
 
 import java.util.List;
 
+import org.serratec.dto.ClienteCadastroDTO;
+import org.serratec.exceptions.ClienteException;
 import org.serratec.model.Cliente;
 import org.serratec.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +43,25 @@ public class ClienteResource {
 		List<Cliente> clientes = clienteRepository.findAll();
 		
 		return new ResponseEntity<>(clientes, HttpStatus.OK);
+	}
+	
+	@PutMapping("/cliente/{id}")
+	public ResponseEntity<?> update(@PathVariable String cpf, @RequestBody ClienteCadastroDTO dto ) throws ClienteException{
+		
+		Cliente cliente = clienteRepository.findByCpf(cpf).orElseThrow(() -> new ClienteException ("Cliente não encontrado"));
+		
+		cliente.setNome(dto.getNome());
+		cliente.setDataNascimento(dto.getDataNascimento());
+		cliente.setEmail(dto.getEmail());
+		cliente.setEndereco(dto.getEndereco().toEndereco());
+		cliente.setSenha(dto.getSenha()); // realizar alteração
+		cliente.setTelefone(dto.getTelefone());
+		cliente.setUsuario(dto.getUsuario());
+		
+		clienteRepository.save(cliente);
+		
+		return new  ResponseEntity<>("Cliente alterado com sucesso",HttpStatus.OK);
+		
+		
 	}
 }
