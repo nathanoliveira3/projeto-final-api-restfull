b0c2d127c8386d2904fc6e5f8439c4ac6ac547c9
Vinicius Gaspar Siqueira <vgsiqueira94@gmail.com>
2021-06-13 17:16:03 -0300

    Alteração para envio de e-mail

--- Loja/src/main/java/org/serratec/enums/StatusPedido.java
+++ Loja/src/main/java/org/serratec/enums/StatusPedido.java
@@ -2,7 +2,10 @@ package org.serratec.enums;
 
 public enum StatusPedido {
 
-	AGUARDANDO_PAGAMENTO, PAGO,
-	ENVIADO, FINALIZADO;	
+	AGUARDANDO_PAGAMENTO,
+	PAGO,
+	ENVIADO,
+	FINALIZADO;
 
-}
+	}
+
--- Loja/src/main/java/org/serratec/resource/PedidoResource.java
+++ Loja/src/main/java/org/serratec/resource/PedidoResource.java
@@ -1,11 +1,14 @@
 package org.serratec.resource;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.serratec.dto.StatusPedidoAlterarDTO;
+import org.serratec.enums.StatusPedido;
 import org.serratec.exceptions.PedidoException;
 import org.serratec.model.Pedido;
 import org.serratec.repository.PedidoRepository;
+import org.serratec.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +22,10 @@ public class PedidoResource {
 
 	@Autowired
 	private PedidoRepository pedidoRepository;
-
+	
+	@Autowired
+	EmailService emailService;
+	
 	@GetMapping("/pedido")
 	public ResponseEntity<?> getPedidos() {
 		List<Pedido> pedidos = pedidoRepository.findAll();
@@ -30,10 +36,23 @@ public class PedidoResource {
 	@PutMapping("/pedido/statusPedido")
 	public ResponseEntity<?> putStatus(@RequestBody StatusPedidoAlterarDTO dto){	
 		
+		
+		
 		Pedido pedido;
 		try {
+			
 			pedido = dto.toPedido(pedidoRepository);
 			pedidoRepository.save(pedido);
+			
+			if (pedido.getStatus().equals(StatusPedido.FINALIZADO))
+				emailService.enviar("Pedido finalizado com sucesso!",
+						"Data de Entrega: " + LocalDate.now().plusDays(15) + 
+						"Produtos: " + pedido.getProdutos(), 
+						pedido.getCliente().getEmail(), 
+						"Capim Canela E-commerce");
+				
+				
+			
 			return new ResponseEntity<>("Pedido alterado com sucesso", HttpStatus.OK);
 		} catch (PedidoException e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
